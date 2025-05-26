43f76219c7ee851740145cae6c0ec698b4acd9f8
Navindra M <84118384+navindra-bit@users.noreply.github.com>
2025-05-26 22:29:36 +0530

    Update 1

---
+++ Quiz interface using RadioButton/RbandCb/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RbandCb
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public int count = 0;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void RadioButton_Click(object sender, RoutedEventArgs e)
+        {
+            RadioButton radioButton = (RadioButton)sender;
+
+            if(radioButton.Tag.ToString() == "C" )
+            {
+                count++;
+                radioButton.Foreground = Brushes.Green;
+            }
+            else
+            {
+                radioButton.Foreground = Brushes.Red;
+            }
+            tl2.Text  = count.ToString();
+            
+            StackPanel stackPanel = (StackPanel)radioButton.Parent;
+            stackPanel.IsEnabled = false;
+            
+        }
+        
+        private void sm_Click(object sender, RoutedEventArgs e)
+        {
+           
+            ScorePage scorePage = new ScorePage(count);
+            scorePage.Show();
+            this.Close();
+        }
+    }
+}
